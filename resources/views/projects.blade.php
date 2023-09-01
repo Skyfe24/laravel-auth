6c5da58a64fe0ce2a5451fd1886fc0088f6b61d3
Skyfe24 <fabio.erase@gmail.com>
2023-09-01 16:36:02 +0200

    created index, show and delete blade

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5">
+    <div class="container py-5 text-primary">
         <div class="logo_laravel">
             <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-25">
                 <g clip-path="url(#clip0)" fill="#EF3B2D">
@@ -10,7 +10,7 @@
                 </g>
             </svg>
         </div>
-        <h1 class="display-5 fw-bold">
+        <h1 class="display-5 fw-bold ">
             QUI SEI SU PROJECTS.BLADE.PHP
         </h1>
 
@@ -20,7 +20,7 @@
 </div>
 
 <div class="content">
-    <div class="container">
+    <div class="container text-primary">
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
     </div>
 </div>
